8d79a3d23c7895f2f2d7ecc5636ec77b7f3d7b23
Michael van de Water <michael.vandewater@sharesies.co.nz>
2021-12-15 20:26:16 +1300

    Day 15 part 1 in progress

--- day-15/advent-of-code-015.py
+++ day-15/advent-of-code-015.py
@@ -1,15 +1,34 @@
+class Vertex:
+    def __init__(self, x, y, value):
+        self.x = x
+        self.y = y
+        self.value = value
+
+    @property
+    def above(self):
+        pass
+
+
 def get_content_from_file():
     with open("puzzle-input.txt") as f:
         content = f.readlines()
-        grid = [x.strip("\n") for x in content]
+        grid_raw = [x.strip("\n") for x in content]
+        
+        grid = []
+        for y in range(len(grid_raw)):
+            row = []
+            for x in range(len(grid_raw[0])):
+                row.append(Vertex(x=x, y=y, value=grid_raw[y][x]))
+            grid.append(row)
+
         return grid
 
 
 def part_one():
     grid = get_content_from_file()
 
-    for line in grid:
-        print(line)
+    for row in grid:
+        print(row)
 
 
 if __name__ == "__main__":
