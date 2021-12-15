90e1e7f61f7a8e87766e3ca51a8ea99a9168db59
Michael van de Water <mjvdw@Beryllium.fritz.box>
2021-12-16 09:33:31 +1300

    Day 13 complete

--- day-13/advent-of-code-013.py
+++ day-13/advent-of-code-013.py
@@ -1,37 +1,87 @@
 class Instruction:
-    def __init__(self, direction, coord):
+    def __init__(self, direction, fold_line):
         self.direction = direction
-        self.coord = coord
+        self.fold_line = fold_line
 
 
 class Grid:
     def __init__(self, coords):
         self.coords = coords
-        self.grid = self.generate_grid_from_coords(self.coords)
+        self.grid = self.generate_grid_from_coords()
 
-    def generate_grid_from_coords(self, coords):
-        height = []
-        width = []
-        
-        for coord in coords:
-            height.append(coord[0])
-            width.append(coord[1])
-
-        max_height = max(height)
-        max_width = max(width)
-
+    def generate_grid_from_coords(self):
         grid = []
-        row = [False] * (max_width + 1)
-        for i in range(max_height + 1):
+        row = ["."] * (self.max_width + 1)
+        for i in range(self.max_height + 1):
             grid.append(row.copy())
 
-        for coord in coords:
-            grid[coord[0]][coord[1]] = True
+        for coord in self.coords:
+            grid[coord[0]][coord[1]] = "#"
 
         return grid
+    
+    def print_grid(self):
+        grid = self.generate_grid_from_coords()
+        grid = list(zip(*grid[::-1]))
+
+        printable_grid = []
+        for y in range(self.min_height, self.max_height + 1):
+            row_str = ""
+            for x in range(self.min_width, self.max_width + 1):
+                row_str += grid[x][y]
+            printable_grid.append(row_str)
+        
+        for row in printable_grid:
+            print(row)
+
+
+    def count_dots(self):
+        coords_as_tuples = [(i[0],i[1]) for i in self.coords]
+        unique = list(dict.fromkeys(coords_as_tuples))
+        count = len(list(unique))
+        return count
+
 
     def fold(self, instruction):
-        pass
+        coord_index = 0 if instruction.direction == "x" else 1
+
+        for i in range(len(self.coords)):
+            value = self.coords[i][coord_index]
+            if value > instruction.fold_line:
+                new_value = value - (2 * (value - instruction.fold_line))
+                self.coords[i][coord_index] = new_value
+    
+    @property
+    def max_height(self):
+        height = []
+        for coord in self.coords:
+            height.append(coord[0])
+        
+        return max(height)
+
+    @property
+    def max_width(self):
+        width = []
+        for coord in self.coords:
+            width.append(coord[1])
+        
+        return max(width)
+
+    @property
+    def min_height(self):
+        height = []
+        for coord in self.coords:
+            height.append(coord[0])
+        
+        return min(height)
+
+    @property
+    def min_width(self):
+        width = []
+        for coord in self.coords:
+            width.append(coord[1])
+        
+        return min(width)
 
 
 def get_content_from_file():
@@ -60,12 +110,25 @@ def get_content_from_file():
 def part_one():
     grid, instructions = get_content_from_file()
 
-    print(grid.grid)
-
     for instruction in instructions:
         grid.fold(instruction)
+        visible_dots = grid.count_dots()
+        print("Answer", visible_dots)
         break    # Break after first iteration just for part one
 
 
+def part_two():
+    grid, instructions = get_content_from_file()
+
+    for instruction in instructions:
+        grid.fold(instruction)
+        
+    grid.print_grid()
+    # print(grid.coords)
+    
+    # Tried EFLFJQRF
+
+
 if __name__ == "__main__":
-    part_one()
+    # part_one()
+    part_two()
